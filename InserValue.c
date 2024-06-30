1cccb1e2e7d80706197b43017700fff5c4a20934
Om Kumar Saket <omkumarsaket86@gmail.com>
2024-06-30 17:42:18 +0530

    first commit

---
+++ InserValue.c
@@ -0,0 +1,10 @@
+
+
+#include <stdio.h>
+int main(){
+    int a = 5;
+     printf("Integer value with positive data: %d\n", a);
+     printf("Character value with: %d\n",'A');
+     printf("Floating value with: %d\n", 3.14);
+    return 0;
+}
